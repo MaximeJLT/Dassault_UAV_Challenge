ac9a790f3639374f830ce34c95557adef5b5b34f
MJ_quant <maxime.jolliot@gmail.com>
2026-06-30 18:13:14 +0200

    Refactor conversion calculations for clarity

--- ml/conversion.py
+++ ml/conversion.py
@@ -23,14 +23,14 @@ def GPS_target(master, x, y, w, h, track_id):
     offset_forward_drone = -altitude * math.tan(math.radian(angle_vertical))
 
     msg = master.recv_match(type='ATTITUDE', blocking=True, timeout=5)
-    uav_yaw = msg.yaw if msg is not None else 0.0   # en radians, message MAVLink lu donnant roll, pitch et yaw  
+    uav_yaw = msg.yaw if msg is not None else 0.0  
 
     cos_y = math.cos(uav_yaw)
     sin_y = math.sin(uav_yaw)
     delta_nord = offset_forward_drone * cos_y - offset_right_drone * sin_y
     delta_est  = offset_forward_drone * sin_y + offset_right_drone * cos_y
 
-    target_lat = lat + delta_nord / 111320 # conversion approximative de mètres en degrés de latitude
-    target_lon = lon + delta_est / (111320 * np.cos(np.radians(lat))) # conversion approximative de mètres en degrés de longitude
+    target_lat = lat + delta_nord / 111320 
+    target_lon = lon + delta_est / (111320 * np.cos(np.radians(lat))) 
 
     return target_lat, target_lon

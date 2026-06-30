083cbb64dbb6c89bf01e3bb139a0228f038d0b85
MJ_quant <maxime.jolliot@gmail.com>
2026-06-30 18:12:50 +0200

    Refactor GPS_target function and clean up imports
    Removed commented-out import and adjusted formatting for field of view variables.

--- ml/conversion.py
+++ ml/conversion.py
@@ -1,35 +1,27 @@
 import numpy as np
 from NN import normalised_coordinates
-#from connection import connect_udp
 from read_gps import get_lat_lon_relalt
 import math
 from ..read_gps import get_lat_lon_relalt
 
 def GPS_target(master, x, y, w, h, track_id):
 
-    #----------------------------Conversion des coordonnées normalisées en pixels en angle dans le repère caméra------------------------------
 
     x_centré = x - 0.5
     y_centré = y - 0.5
 
-    # fov a mesurer sur notre camera
 
-    fov_horizontal = 90  # champ de vision horizontal de la caméra en degrés
-    fov_vertical = 60    # champ de vision vertical de la caméra en degrés
+    fov_horizontal = 90  
+    fov_vertical = 60    
 
     angle_horizontal = x_centré * fov_horizontal
     angle_vertical = y_centré * fov_vertical
 
-    #----------------------------Conversion des angles caméra en angle monde-------------------------------------------------------------------
-
-    # Altitude AGL (car pas de LiDAR)
     lat, lon, altitude = get_lat_lon_relalt(master)
 
-    #offset au sol dans le repere drone (trigo)
     offset_right_drone = altitude * math.tan(math.radians(angle_horizontal))
     offset_forward_drone = -altitude * math.tan(math.radian(angle_vertical))
 
-    # Rotation par le yaw UAV pour passer en repère NORD/EST monde
     msg = master.recv_match(type='ATTITUDE', blocking=True, timeout=5)
     uav_yaw = msg.yaw if msg is not None else 0.0   # en radians, message MAVLink lu donnant roll, pitch et yaw  
 
